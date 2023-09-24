6d1a492c86f828b6fe9e47c51fe65e4becd6271c
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-09-24 13:03:37 +0530

    arrow_function

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@
 import express from "express";
 const app = express();
 
-app.get('/users', function (req, res) {
+app.get('/users', (req, res) => {
     res.json({
         data: 'Raja Kumar Kamal',
     });
 });
 
-app.listen(8000, function () {
+app.listen(8000,  () => {
     console.log('Node server is running on port 8000');
 });
