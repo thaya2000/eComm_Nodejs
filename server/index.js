071025a8f72874fc37580bbe88ef658de41c4e9b
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-09-24 19:44:01 +0530

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -2,19 +2,25 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    res.json({
-        data: 'Raja Kumar Kamal',
-    });
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ecommerce2022")
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Connection Error: ", err));
+
+app.get("/users", (req, res) => {
+  res.json({
+    data: "Raja Kumar Kamal",
+  });
 });
 
 const port = process.env.PORT;
 
-app.listen(port,  () => {
-    console.log(`Node server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Node server is running on port ${port}`);
 });
