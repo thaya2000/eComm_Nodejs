570023b9623335700278092aaf0b77556858c3ff
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-09-27 01:27:54 +0530

    create login route and login validation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -59,3 +59,44 @@ export const register = async (req, res) => {
     console.log(err);
   }
 };
+
+
+
+export const login = async (req, res) => {
+  try {
+    // 1. destructure name, email, password from req.body
+    const { email, password } = req.body;
+    // 2. all fields require validation
+    if (!email) {
+      return res.json({ error: "Email is required" });
+    }
+    if (!password || password.length < 6) {
+      return res.json({ error: "Password must be at least 6 characters long" });
+    }
+    // 3. check if email is taken
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ error: "User not found" });
+    }
+    // 4. compare password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.json({ error: "Wrong password" });
+    }
+    // 5. create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // 7. send response
+    res.json({
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
